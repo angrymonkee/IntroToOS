e50b208e1f2950c5fb7cb3d78fa641e863533d55
angrymonkee <daniel.jones.r@gmail.com>
2015-09-06 16:19:10 -0400

    initial check-in for Part1

---
+++ Projects/Project1/Part1/gfclient.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h> 
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include "gfclient.h"
+
+
+gfcrequest_t *gfc_create()
+{
+	struct gfcrequest_ ret = malloc(sizeof(gfcrequest_));
+	return ret;
+}
+
+void gfc_set_server(gfcrequest_t *gfr, char* server)
+{
+	gfr->ServerLocation = server;
+}
+
+void gfc_set_path(gfcrequest_t *gfr, char* path)
+{
+	gfr->Path = path;
+}
+
+void gfc_set_port(gfcrequest_t *gfr, unsigned short port)
+{
+	gfr->Port = port;
+}
+
+void gfc_set_headerfunc(gfcrequest_t *gfr, void (*headerfunc)(void*, size_t, void *))
+{
+	// not sure what to do here yet
+}
+
+void gfc_set_headerarg(gfcrequest_t *gfr, void *headerarg)
+{
+	
+}
+
+void gfc_set_writefunc(gfcrequest_t *gfr, void (*writefunc)(void*, size_t, void *))
+{
+  
+}
+
+void gfc_set_writearg(gfcrequest_t *gfr, void *writearg)
+{
+  
+}
+
+int gfc_perform(gfcrequest_t *gfr)
+{
+  
+}
+
+gfstatus_t gfc_get_status(gfcrequest_t *gfr)
+{
+	
+}
+
+char* gfc_strstatus(gfstatus_t status)
+{
+
+}
+
+size_t gfc_get_filelen(gfcrequest_t *gfr)
+{
+
+}
+
+size_t gfc_get_bytesreceived(gfcrequest_t *gfr)
+{
+
+}
+
+void gfc_cleanup(gfcrequest_t *gfr)
+{
+	free(gfr);
+}
+
+void gfc_global_init()
+{
+}
+
+void gfc_global_cleanup()
+{
+}
